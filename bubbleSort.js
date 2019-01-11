4a0fccb0ece3acb2be6955dca9f73c9a88ecf2b8
Brian Lafuente <loulafuente@MacBook-Pro-5.local>
2019-01-11 15:24:19 -0500

    bubbleSort init

---
+++ bubbleSort.js
@@ -0,0 +1,23 @@
+let myList = [7,6,2,9,2,8];
+
+console.log(myList);
+// let temp = myList[0];// temp = 7
+// myList[0] = myList[1]; // myList[0] = 6
+// myList[1] = temp; // myList[1] = 7
+
+let origList = myList;
+
+// console.log(myList);
+sort = false;
+// while(!sort){
+    for (k = myList.length-1; k > 0; k--){
+        for (i = 0; i < myList.length-1; i++){
+            if (myList[i] > myList[i+1]){
+            temp = myList[i];
+            myList[i] = myList[i+1];
+            myList[i+1] = temp;
+            }
+        }
+        // console.log(myList);
+    }
+    console.log(myList);
